b0db363b6adc8cac0df8fdae57744b53b50b762a
Matt Thorne <matt.thorne1982@gmail.com>
2024-12-07 16:26:55 +0000

    Updating file structure
    Changed from gzip to tar.
    Altered deletion procoess.

--- logArchive.sh
+++ logArchive.sh
@@ -2,28 +2,25 @@
 
 #Setting variable names
 folderName=$1
-currentDate=$(date +"%Y%m%d" -u)
-currentBackupDate=$(date --date="Yesterday" +"%Y-%m-%d" -u)
-deleteDate=$(date --date="14 days ago" +"%Y-%m-%d" -u)
+currentDate=$(date +"%Y%m%d-%H%m%S" -u)
+#Update the folder name
+$backupFolder="/media/matt/backup"
+
 
 #Defining function to compress and move logs, then remove old logs
 rotateFunction () {
-    gzip /var/log/$folderName/access.$currentBackupDate.log
-    gzip /var/log/$folderName/error.$currentBackupDate.log
+    #Update the folder name
+    tar -czvf $backupFolder/$currentDate.tgz $folderName/*.log
 
-    cp /var/log/$folderName/access.$currentBackupDate.log /mnt/backup
-    cp /var/log/$folderName/error.$currentBackupDate.log /mnt/backup
+    find $backupFolder/ -type f -mtime +7 -name '*.tgz' -execdir rm -- '{}' \;
 
-    rm /var/log/$folderName/access.$deleteDate-*.log.gz
-    rm /var/log/$folderName/error.$deleteDate-*.log.gz
-
-    echo "$currentDate - Backup completed" >> /backup/logRotate.log;
+    echo "$currentDate - Backup completed" >> $backupFolder/logArchive.log;
 }
 
 
 #Check if a folder name has been passed into the arg then rotate logs if it has been supplied
 if [ -z "$1" ]
-    then echo "$currentDate - No file path entered" >> /backup/logRotate.log;
+    then echo "$currentDate - No file path entered" 2>&1 | tee -a $backupFolder/logArchive.log;
     else rotateFunction;
 fi
 
